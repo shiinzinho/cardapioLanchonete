3a7f7404bc6e8940ed7abd852b7cc6e3d347e61d
shiinzinho <gabrieldosantoslacerda@gmail.com>
2024-04-04 15:34:38 -0300

    UPDATE

--- App.tsx
+++ App.tsx
@@ -3,10 +3,11 @@ import { FlatList } from "react-native";
 import MenuList from "./src/snackBar/MenuList";
 import Cart from "./src/snackBar/Cart";
 import CadastroProduto from "./src/screens/CadastroProduto";
+import CadastroCliente from "./src/screens/CadastroCliente";
 
 function App(): React.JSX.Element {
   return(
-    <CadastroProduto />
+    <CadastroCliente />
   );
 }
 export default App;
